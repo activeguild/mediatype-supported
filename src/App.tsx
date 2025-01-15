cb5db9b8c3d897679be5a22a5cb40c43fbb0c771
j1ngzoue <workdrive0306@gmail.com>
2025-01-15 19:13:04 +0900

    chore:

--- src/App.tsx
+++ src/App.tsx
@@ -60,8 +60,8 @@ export default function App() {
   return (
     <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
       <h1>MediaRecorder MIMEタイプ確認</h1>
-      <strong>video/mp4; codecs="avc1": </strong>{`${MediaRecorder.isTypeSupported('video/mp4; codecs="avc1"')}`}<br />
       <strong>video/mp4;:</strong> {`${MediaRecorder.isTypeSupported('video/mp4;')}`}<br />
+      <strong>video/mp4; codecs="avc1": </strong>{`${MediaRecorder.isTypeSupported('video/mp4; codecs="avc1"')}`}<br />
       <strong>video/mp4; codecs="avc1.42E01E":</strong> {`${MediaRecorder.isTypeSupported('video/mp4; codecs="avc1.42E01E"')}`}<br />
       <strong>video/webm; codecs="vp8":</strong> {`${MediaRecorder.isTypeSupported('video/webm; codecs="vp8"')}`}<br />
       <strong>video/webm; codecs="vp9":</strong> {`${MediaRecorder.isTypeSupported('video/webm; codecs="vp9"')}`}<br />
